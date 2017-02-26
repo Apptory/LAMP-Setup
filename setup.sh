57cfbc6693c3d4058a9e28b17687363c4efa523c
Idan Yalovich <idan@ystudiodesign.com>
2017-02-26 11:08:49 +0200

    Adding GIT andPermissions

--- setup.sh
+++ setup.sh
@@ -30,3 +30,13 @@ sudo a2enmod rewrite
 
 # Reloading the APP
 sudo /etc/init.d/apache2 restart
+
+# installing GIT
+sudo apt-get install git
+
+sudo chown -R root:www-data /var/www
+sudo chmod -R 775 /var/www
+
+sudo usermod -a -G www-data ubuntu
+
+ln -s /var/www/ ~/www
